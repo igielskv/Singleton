75bba17be33b5b6cf803399f8b2c5285d1c133ae
Manoli <igielskv@icloud.com>
2020-10-28 08:50:26 +0100

    Initial Commit

---
+++ Singleton/SingletonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SingletonApp.swift
+//  Singleton
+//
+//  Created by Manoli on 28/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct SingletonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
